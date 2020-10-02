0736b8150fc5c9af047d39fb6dc527baebf6d52b
mengdidi <441348693@qq.com>
2020-10-02 12:43:10 -0700

    small change

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Mengdi Li', // Title for your website.
     tagline: 'I am facilitating a workshop for vGHC20!',
     profilePhoto: 'img/profile.png',
-    url: 'https://rosemary-at-ghc.github.io', // Your website URL
+    url: 'https://mengdidi.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
